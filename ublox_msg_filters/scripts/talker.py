337b7581b3157dcd5c14ed5fb1cbfab0d136fee8
Magnus Berdal <magnus.berdal@heimdallpower.com>
2025-11-21 19:57:13 +0000

    Example runs

--- ublox_msg_filters/scripts/talker.py
+++ ublox_msg_filters/scripts/talker.py
@@ -1,37 +1,40 @@
 #!/usr/bin/python3
-import rospy
+import rclpy
+from rclpy.node import Node
 from ublox_msgs.msg import NavHPPOSLLH, NavRELPOSNED9, NavVELNED
 
-class Talker(object):
+class Talker(Node):
     def __init__(self):
+        super().__init__('talker')
         self.itow = 0
-        self.pub1 = rospy.Publisher('msg1', NavHPPOSLLH, queue_size=2)
-        self.pub2 = rospy.Publisher('msg2', NavRELPOSNED9, queue_size=2)
-        self.pub3 = rospy.Publisher('msg3', NavVELNED, queue_size=2)
-        self.timer = rospy.Timer(rospy.Duration(0.2),self.publish)
+        self.pub1 = self.create_publisher(NavHPPOSLLH, 'msg1', 2)
+        self.pub2 = self.create_publisher(NavRELPOSNED9, 'msg2', 2)
+        self.pub3 = self.create_publisher(NavVELNED, 'msg3', 2)
+        self.timer = self.create_timer(0.2, self.publish)
 
-    def publish(self, event):
-        rospy.loginfo("TX %u", self.itow)
-
+    def publish(self):
+        self.get_logger().info("TX %u" % self.itow)
         msg1 = NavHPPOSLLH()
-        msg1.iTOW = self.itow
+        msg1.i_tow = self.itow
         self.pub1.publish(msg1)
 
         msg2 = NavRELPOSNED9()
-        msg2.iTOW = self.itow
+        msg2.i_tow = self.itow
         self.pub2.publish(msg2)
 
         msg3 = NavVELNED()
-        msg3.iTOW = self.itow
+        msg3.i_tow = self.itow
         self.pub3.publish(msg3)
 
         self.itow += 1
 
 def main():
-    rospy.init_node('talker')
+    rclpy.init()
     talker = Talker()
-    rospy.loginfo("Ready to transmit")
-    rospy.spin()
+    talker.get_logger().info("Ready to transmit")
+    rclpy.spin(talker)
+    talker.destroy_node()
+    rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
